b1fb29b284f7f69e68b465f290b79a0c576f6bd8
露乐ios <718401263@qq.com>
2018-04-17 18:48:47 +0800

    no message

---
+++ api/api.js
@@ -0,0 +1,36 @@
+const host = 'http://ws.mammasay.com'
+const wxRequest = (params, url) => {
+  wx.showToast({
+    title: '加载中',
+    icon: 'loading'
+  })
+  wx.request({
+    url: url,
+    method: params.method || 'GET',
+    data: params.data || {},
+    header: {
+      'Content-Type': 'application/json'
+    },
+    success: (res) => {
+      params.success && params.success(res)
+      wx.hideToast()
+    },
+    fail: (res) => {
+      params.fail && params.fail(res)
+    },
+    complete: (res) => {
+      params.complete && params.complete(res)
+    }
+  })
+}
+
+// Index
+const getVolById = (params) => wxRequest(params, host + '/api/hp/detail/' + params.query.id)
+
+// community page
+const apiForCommunityPage = (params) => wxRequest(params, host + '/api/cms/lelch/communityPage')
+
+module.exports = {
+  getVolById,
+  apiForCommunityPage
+}
---
+++ pages/community/index.js
@@ -0,0 +1,94 @@
+
+var api = require('../../api/api.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    BannerList : []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getCommunityRecommandPage()
+  },
+  /**
+   * 加载社区推荐数据
+   */
+  getCommunityRecommandPage : function(){
+    let that = this
+    api.apiForCommunityPage({
+      success: (res) => {
+        if (res.data.Code == 0){
+          this.setData({ BannerList: res.data.Data.BannerList })
+        }
+      }
+    })
+  },
+  /**
+   * 社区item点击事件
+   */
+  activityItemTapAction:function(e){
+    
+    console.log('activityItemTapAction' + e)
+    let itemModel = e.currentTarget.dataset.contentmodel
+
+    wx.navigateTo({
+      url: '../webView/CommunitywebView?content='+itemModel.Content,
+    })
+  
+
+  } ,
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})

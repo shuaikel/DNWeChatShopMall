9fc875cf9ba5278bb54e55db68aa9855536f4cc3
露乐ios <718401263@qq.com>
2018-04-23 18:17:50 +0800

    no message

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,30 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    dateModel : {} ,
+    BannerList :[],
+    SeckillList : [],
+    ActivityList : [],
+    HotList : [],
+    BannerpageList:[],
+    RecommendList : [],
+
+    productCategoryList: [{
+      "ID": 0,
+      "TenantID": 1,
+      "Name": "首页",
+      "Picture": "https://static.mammasay.com/image/20171102/6364522992248564746252872.jpg",
+      "PageSize": 100,
+      "ParentID": 0,
+      "Level": 0,
+      "Childs": null
+    }],
+
+    userSelectCheckCategoryIndex: 0,
+
+    main_actionArray : ['商品分类','扫码积分','防伪追溯','露安适社区']
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,52 +39,53 @@ Page({
     // })
   },
   onLoad: function () {
+    this.getProductCategoryInfo()
     this.getUserMainPageInfo()
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse){
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  // 获取商品分类类别
+  getProductCategoryInfo : function(e){
+    var date = new Date().getTime()
+    api.apiForCategory({
+      method: "POST",
+      data: {
+        TenantID: 1,
+        ID: 0
+      },
+      query: {
+        date: date
+      },
+      success: (res) => {
+        var tempCategoryList = this.data.productCategoryList
+        tempCategoryList = tempCategoryList.concat(res.data.Data)
+        this.setData({ productCategoryList: tempCategoryList })
+      }
     })
   },
   // 获取首页数据
   getUserMainPageInfo : function(e){
+    var that = this
     var date = new Date().getTime()
     api.apiForMainPageDataSumInfo({
       query:{
         date : date
       },
       success:(res)=>{
-        debugger
+        if(res.data.Code == 0){
+          that.setData({ 
+            BannerList: res.data.Data.BannerList,
+            SeckillList: res.data.Data.Seckill.list,
+            ActivityList: res.data.Data.ActivityList,
+            HotList: res.data.Data.HotList,
+            BannerpageList: res.data.Data.BannerpageList,
+            RecommendList: res.data.Data.RecommendList})
+        }
       }
     })
+  },
+  //  用户切换查看类别
+  userSelectCategoryChangeAction:function(e){
+    let index = e.currentTarget.dataset.index
+    this.setData({ userSelectCheckCategoryIndex: index})
   }
 })
